9af8cbf808615b521d07eef3137b77d698056e60
LizaSber <liza.plesovskaya98@gmail.com>
2025-07-07 23:37:11 +0300

    тест: объявление примитивных типов данных

--- Vvedenie/Task_1_2_4.java
+++ Vvedenie/Task_1_2_4.java
@@ -1,4 +1,17 @@
 package Vvedenie;
 
 public class Task_1_2_4 {
+    public static void main(String [] args){
+    byte varbyte = 22;
+    short varshort = 3343;
+    long varlong = 333333;
+    double vardoubl = 2.4;
+    float varfloat = 4.7F;
+    char varchar = '\u0500';
+    int verint = 3;
+    boolean varboolean = true;
+
+    long varlong2 = 1000000000000000L;
+    float varfloat2 = 2.9F;
+    }
 }
