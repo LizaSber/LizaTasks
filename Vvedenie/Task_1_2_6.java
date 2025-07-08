41844069c47c332b665acb3a6cbb013aa991ee20
LizaSber <liza.plesovskaya98@gmail.com>
2025-07-09 00:36:33 +0300

    тест: арифметические функции (исправление замечаний)

--- Vvedenie/Task_1_2_6.java
+++ Vvedenie/Task_1_2_6.java
@@ -2,8 +2,6 @@ package Vvedenie;
 
 public class Task_1_2_6 {
         public int drawisMonitorsCounter(int monitors, int programmers) {
-            monitors = 100;
-            programmers = 65;
             int extraMonitor = monitors % programmers;
             System.out.println(extraMonitor);
             return extraMonitor;
