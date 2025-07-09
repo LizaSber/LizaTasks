e3961e6607f0a64342d48b8e3d69a2328ffbd896
LizaSber <liza.plesovskaya98@gmail.com>
2025-07-09 20:55:57 +0300

    тест: арифметические функции

--- Vvedenie/Task_1_2_7.java
+++ Vvedenie/Task_1_2_7.java
@@ -3,7 +3,7 @@ package Vvedenie;
 public class Task_1_2_7 {
     public double priceCalculation(double price, int count) {
 
-        int sumPrice = (int) (price * count);
+        double sumPrice = price * count;
 
         System.out.println("Сумма стоимости товаров = " + sumPrice);
 
