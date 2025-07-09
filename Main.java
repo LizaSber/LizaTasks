70d511c3ed5ff9914aafcc6ebfd4817c82bcb205
LizaSber <liza.plesovskaya98@gmail.com>
2025-07-09 20:43:02 +0300

    тест: арифметические функции

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import Vvedenie.Task_1_2_7;
+
 public class Main {
     public static void main(String[] args) {
 
+        Task_1_2_7 taskObj = new Task_1_2_7();
+
+        taskObj.priceCalculation(55.5, 67);
     }
 }
---
+++ Vvedenie/Task_1_2_7.java
@@ -0,0 +1,12 @@
+package Vvedenie;
+
+public class Task_1_2_7 {
+    public double priceCalculation(double price, int count) {
+
+        int sumPrice = (int) (price * count);
+
+        System.out.println("Сумма стоимости товаров = " + sumPrice);
+
+        return price;
+    }
+}
